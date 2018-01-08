0b45b0c8a304b9bf2e55fbaa0ea49401cdcdbb49
sebastian.schmidt <bastman77@gmail.com>
2018-01-08 07:58:24 +0100

    initial

--- rest-api/src/main/kotlin/com/example/graphql/familytree/Person.kt
+++ rest-api/src/main/kotlin/com/example/graphql/familytree/Person.kt
@@ -1,6 +1,9 @@
 package com.example.graphql.familytree
 
+import java.time.Instant
+
 data class Person(
         val id: String,
-        val name: String
+        val name: String,
+        val createdAt: Instant
 )
--- rest-api/src/main/kotlin/com/example/graphql/familytree/PersonDao.kt
+++ rest-api/src/main/kotlin/com/example/graphql/familytree/PersonDao.kt
@@ -1,21 +1,22 @@
 package com.example.graphql.familytree
 
 import org.springframework.stereotype.Component
+import java.time.Instant
 import java.util.*
 
 @Component
 class PersonDao {
     private val data = mutableListOf(
-            Person(id = "qeii", name = "Queen Elizabeth II"),
-            Person(id = "pp", name = "Prince Philip"),
-            Person(id = "pc", name = "Prince Charles"),
-            Person(id = "pw", name = "Prince William")
+            Person(id = "qeii", name = "Queen Elizabeth II", createdAt = Instant.now()),
+            Person(id = "pp", name = "Prince Philip", createdAt = Instant.now()),
+            Person(id = "pc", name = "Prince Charles", createdAt = Instant.now()),
+            Person(id = "pw", name = "Prince William", createdAt = Instant.now())
     )
 
     fun getPersonById(id: String) = data.firstOrNull { person -> person.id == id }
 
     fun createPerson(name: String): Person {
-        val person = Person(id = UUID.randomUUID().toString(), name = name)
+        val person = Person(id = UUID.randomUUID().toString(), name = name, createdAt = Instant.now())
         data.add(person)
         return person
     }
